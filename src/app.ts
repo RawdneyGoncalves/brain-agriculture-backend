3d0f5c5be0b20bb3725b30ac2676e7358da10a09
Rawdney Mendes <mendesrawdney@gmail.com>
2024-01-12 05:17:40 -0300

    initial kubernets

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ import logger from './utils/logger';
 import config from './config/config';
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(helmet());
