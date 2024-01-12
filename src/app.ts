c021cbee8292fab6af007606f86716c3fc028103
Rawdney Mendes <mendesrawdney@gmail.com>
2024-01-12 03:13:07 -0300

    development aplication

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,37 @@
-// ./app.ts
-
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import helmet from 'helmet';
-import sequelize from './config/config'; 
+import sequelize from './config/config';
 import producerRoutes from './routes/producer';
 import errorHandler from './middleware/errorHandler';
 import logger from './utils/logger';
+import config from './config/config';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(helmet());
 app.use(bodyParser.json());
 
-sequelize.sync();
+(async () => {
+  try {
+    await sequelize.sync();
+    logger.info('Conexão com o banco de dados estabelecida com sucesso');
+  } catch (error) {
+    console.error('Erro ao sincronizar o banco de dados:', error);
+  }
 
-app.use('/producers', producerRoutes);
+  app.use('/producers', producerRoutes);
 
-app.use(errorHandler);
+  app.use(errorHandler);
 
-app.listen(PORT, () => {
-  logger.info(`Servidor iniciado na porta ${PORT}`);
-});
+  try {
+    app.listen(PORT, () => {
+      logger.info(`Servidor iniciado na porta ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Erro ao iniciar o servidor:', error);
+  }
+})();
