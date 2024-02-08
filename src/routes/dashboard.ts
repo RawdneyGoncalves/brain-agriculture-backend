bdab90bb374f2786ff5c05dc45bfddc12ba501f0
Rawdney Mendes <mendesrawdney@gmail.com>
2024-02-08 11:17:21 -0300

    fix bug and dashboard

--- src/routes/dashboard.ts
+++ src/routes/dashboard.ts
@@ -16,14 +16,5 @@ const router = express.Router();
  *             example:
  *               message: "Dados do dashboard obtidos com sucesso"
  */
-router.get("/", async (_req: Request, res: Response) => {
-  try {
-    const dashboardData = await getDashboardDataController(_req, res);
-    res.status(200).json(dashboardData);
-  } catch (error) {
-    console.error("Erro ao obter dados do dashboard:", error);
-    res.status(500).json({ error: "Erro ao obter dados do dashboard" });
-  }
-});
-
+router.get("/", getDashboardDataController) 
 export default router;
